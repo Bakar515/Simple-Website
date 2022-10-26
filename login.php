9e63d4d64424a9df392f5857860bc891d7fb24f6
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-10-26 10:18:19 +0500

    css files add

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>User Registeration And Login</title>
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="Css/bootstrap.css">
     </head>
     <body>
         <div class="container">
