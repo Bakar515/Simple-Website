d08c7a2cba4054ab95a6c1ed75ee204c59f7c471
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-10-26 17:09:39 +0500

    Signup And Login

--- login.php
+++ login.php
@@ -1,48 +1,71 @@
-<html>
-    <head>
-        <title>User Registeration And Login</title>
-        <link rel="stylesheet" type="text/css" href="Css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="Css/style.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="login-box">
-                <div class="row">
-                    <div class="col-md-6 login-left">
-                        <h2>Login Here</h2>
-                        <form action="validation.php" method="POST">
-                            <div class="form-group">
-                                <label>Username</label>
-                                <input type="text" name="user" class="form-control" required>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label>Password</label>
-                                <input type="password" name="password" class="form-control" required>
-                            </div>
-                            <br>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </form>
-                    </div>
+<?php
 
-                    <div class="col-md-6 login-right">
-                        <h2>Register Here</h2>
-                        <form action="validation.php" method="POST">
-                            <div class="form-group">
-                                <label>Username</label>
-                                <input type="text" name="user" class="form-control" required>
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label>Password</label>
-                                <input type="password" name="password" class="form-control" required>
-                            </div>
-                            <br>
-                            <button type="submit" class="btn btn-primary">Register</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    if ($user) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: index.php");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    
+    <h1>Login</h1>
+    
+    <?php if ($is_invalid): ?>
+        <em>Invalid login</em>
+    <?php endif; ?>
+    
+    <form method="post">
+        <label for="email">email</label>
+        <input type="email" name="email" id="email"
+               value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        
+        <button>Log in</button>
+    </form>
+    
+</body>
 </html>
+
+
+
+
+
+
+
+
