4a627a5c8b5e817d77f430b96521cd304c1e90fa
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-10-26 10:06:41 +0500

    Add code

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>User Registeration And Login</title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Login Here</h2>
+                    <form action="validation.php" method="POST"
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
