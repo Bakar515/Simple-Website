d0d35070e7bbbb6ebfda364b93e4adc35156408d
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-10-26 10:35:04 +0500

    Register create

--- login.php
+++ login.php
@@ -22,6 +22,23 @@
                         <button type="submit" class="btn btn-primary">Login</button>
                     </form>
                 </div>
+
+                <div class="col-md-6">
+                    <h2>Register Here</h2>
+                    <form action="validation.php" method="POST">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="user" class="form-control" required>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </form>
+                </div>
             </div>
         </div>
     </body>
