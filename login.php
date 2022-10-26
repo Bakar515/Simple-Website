eab24133ca287d02aed27aecff325ef8b9d2b6f9
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-10-26 10:25:05 +0500

    btn create

--- login.php
+++ login.php
@@ -8,7 +8,17 @@
             <div class="row">
                 <div class="col-md-6">
                     <h2>Login Here</h2>
-                    <form action="validation.php" method="POST"
+                    <form action="validation.php" method="POST">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="user" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class btn btn-primary>Login</button>
+                    </form>
                 </div>
             </div>
         </div>
