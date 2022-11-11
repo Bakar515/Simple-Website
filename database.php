4de2fca7e1c51014295c7e77a211ff874ba70a75
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-11-11 17:00:55 +0500

    changes

--- database.php
+++ database.php
@@ -6,9 +6,9 @@
     $password = "xxx";
 
     $mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                     database: $dbname);
+                        username: $username,
+                        password: $password,
+                        database: $dbname);
                      
     if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
