d5343af43eb154d4529dc15e1702c8e06558afe0
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-11-12 18:06:07 +0500

    connecting gallery-app to login

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-echo
+
 
 	include("connection.php");
 	include("functions.php");
---
+++ jquery.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $("#div1show").click(function() {
+        $("#camera").css("display","none");
+        $("#headphone").css("display","none");
+        $("#phone").css("display","none");
+        $("#watch").css("display","none");
+        $("#shoe").css("display","none");
+        $("#all").css("display","block");
+    });
+});
+$(document).ready(function() {
+    $("#div2show").click(function() {
+        $("#all").css("display","none");
+        $("#headphone").css("display","none");
+        $("#phone").css("display","none");
+        $("#shoe").css("display","none");
+        $("#watch").css("display","none");
+        $("#camera").css("display","block");
+    });
+});
+$(document).ready(function() {
+    $("#div3show").click(function() {
+        $("#all").css("display","none");
+        $("#camera").css("display","none");
+        $("#phone").css("display","none");
+        $("#shoe").css("display","none");
+        $("#watch").css("display","none");
+        $("#headphone").css("display","block");
+    });
+});
+$(document).ready(function() {
+    $("#div4show").click(function() {
+        $("#all").css("display","none");
+        $("#camera").css("display","none");
+        $("#headphone").css("display","none");
+        $("#shoe").css("display","none");
+        $("#watch").css("display","none");
+        $("#phone").css("display","block");
+    });
+});
+$(document).ready(function() {
+    $("#div5show").click(function() {
+        $("#all").css("display","none");
+        $("#camera").css("display","none");
+        $("#phone").css("display","none");
+        $("#headphone").css("display","none");
+        $("#watch").css("display","none");
+        $("#shoe").css("display","block");
+    });
+});
+$(document).ready(function() {
+    $("#div6show").click(function() {
+        $("#all").css("display","none");
+        $("#camera").css("display","none");
+        $("#phone").css("display","none");
+        $("#shoe").css("display","none");
+        $("#headphone").css("display","none");
+        $("#watch").css("display","block");
+    });
+});
